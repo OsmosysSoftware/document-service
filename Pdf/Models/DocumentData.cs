03bfb316fbb47f7fbfba53b726716ee5bb379206
PrayatOsm <prayatatman.p@osmosys.co>
2023-08-24 14:22:26 +0530

    adding a pdf namespace

---
+++ Pdf/Models/DocumentData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentService.Pdf.Models
+{
+    public class DocumentData
+    {
+        public List<ContentData> Placeholders { get; set; }
+    }
+}
---
+++ Pdf/PdfDocumentGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentService.Pdf
+{
+    public class PdfDocumentGenerator
+    {
+        public static generatePdfByTemplate(string templatePath)
+        {
+            try 
+            { 
+                 
+            } 
+            catch(Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
