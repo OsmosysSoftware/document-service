1a2fd3065c2fed52694b6cfb75031b58c05eafb6
Sameer <sameer.shaik@osmosys.co>
2023-11-08 15:44:16 +0530

    Fix inclusion of the Web API project properly

--- DocumentServiceWebAPI/Controllers/DocumentController.cs
+++ DocumentServiceWebAPI/Controllers/DocumentController.cs
@@ -1,88 +1,83 @@
 ﻿using DocumentServiceWebAPI.Models;
 using DocumentService.Pdf;
-using DocumentService.Pdf.Models;
 using DocumentService.Word;
-using DocumentService.Word.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using DocumentServiceWebAPI.Helpers;
-using System.Buffers.Text;
 
-namespace DocumentServiceWebAPI.Controllers
+namespace DocumentServiceWebAPI.Controllers;
+
+[Route("api")]
+[ApiController]
+public class DocumentController : ControllerBase
 {
-    [Route("api")]
-    [ApiController]
-    public class DocumentController : ControllerBase
+    string flag;
+    [HttpPost]
+    [Route("pdf/GeneratePdfUsingHtml")]
+    //public IActionResult GeneratePdf(string templatePath, List<ContentMetaData> metaDataList, string outputFilePath)
+    public IActionResult GeneratePdf(PdfGenerationRequestDTO request)
     {
-        string flag;
-        [HttpPost]
-        [Route("pdf/GeneratePdfUsingHtml")]
-        //public IActionResult GeneratePdf(string templatePath, List<ContentMetaData> metaDataList, string outputFilePath)
-        public IActionResult GeneratePdf(PdfGenerationRequestDTO request)
+        try
         {
-            try
+            flag = "pdf";
+            //decoding: passed encoded base64 string to file and saving it to the temp folder and returning that template file's link
+            string templatePath = Base64Helper.ConvertBase64ToFile(request.Base64, flag); 
+
+            // Generate the PDF using the template
+            string outputFolder = Path.Combine(Directory.GetCurrentDirectory(), "convertedPdfs");
+            // Creating a directory for the converted output PDFs if it doesn't exist
+            if (!Directory.Exists(outputFolder))
             {
-                flag = "pdf";
-                //decoding: passed encoded base64 string to file and saving it to the temp folder and returning that template file's link
-                string templatePath = Base64Helper.ConvertBase64ToFile(request.Base64, flag); 
+                Directory.CreateDirectory(outputFolder);
+            }
 
-                // Generate the PDF using the template
-                string outputFolder = Path.Combine(Directory.GetCurrentDirectory(), "convertedPdfs");
-                // Creating a directory for the converted output PDFs if it doesn't exist
-                if (!Directory.Exists(outputFolder))
-                {
-                    Directory.CreateDirectory(outputFolder);
-                }
-
-                string outputFilePath = Path.Combine(outputFolder, "pdfOutput.pdf");
+            string outputFilePath = Path.Combine(outputFolder, "pdfOutput.pdf");
 
 
-                // passing in main function template to pdf.
-                string toolFolderAbsolutePath = "F:\\Desktop\\document-service\\DocumentServiceWebAPI\\Tools\\wkhtmltopdf.exe";
-                PdfDocumentGenerator.GeneratePdfByTemplate(toolFolderAbsolutePath, templatePath, request.DocumentData.Placeholders, outputFilePath);
-                string base64 = Base64Helper.ConvertFileToBase64(outputFilePath);
-                return Ok(new { message = "PDF generated successfully" , Base64 = base64});
+            // passing in main function template to pdf.
+            string toolFolderAbsolutePath = "F:\\Desktop\\document-service\\DocumentServiceWebAPI\\Tools\\wkhtmltopdf.exe";
+            PdfDocumentGenerator.GeneratePdfByTemplate(toolFolderAbsolutePath, templatePath, request.DocumentData.Placeholders, outputFilePath);
+            string base64 = Base64Helper.ConvertFileToBase64(outputFilePath);
+            return Ok(new { message = "PDF generated successfully" , Base64 = base64});
                 
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"PDF generation failed: {ex.Message}");
-            }
         }
-
-        [HttpPost]
-        [Route("word/GenerateWordDocument")]
-        public IActionResult GenerateWord(WordGenerationRequestDTO request)
+        catch (Exception ex)
         {
-            try
+            return BadRequest($"PDF generation failed: {ex.Message}");
+        }
+    }
+
+    [HttpPost]
+    [Route("word/GenerateWordDocument")]
+    public IActionResult GenerateWord(WordGenerationRequestDTO request)
+    {
+        try
+        {
+            flag = "word";
+            string templateFilePath = Base64Helper.ConvertBase64ToFile(request.Base64, flag);
+            DocumentService.Word.Models.DocumentData documentData = new()
             {
-                flag = "word";
-                string templateFilePath = Base64Helper.ConvertBase64ToFile(request.Base64, flag);
-                DocumentService.Word.Models.DocumentData documentData = new()
-                {
-                    Placeholders = request.DocumentData.Placeholders,
-                    TablesData = request.DocumentData.TablesData
-                };
-                // Generate the doc using the word template
-                string outputFolder = Path.Combine(Directory.GetCurrentDirectory(), "convertedDocs");
-                // Creating a directory for the converted output PDFs if it doesn't exist
-                if (!Directory.Exists(outputFolder))
-                {
-                    Directory.CreateDirectory(outputFolder);
-                }
-
-                string outputFilePath = Path.Combine(outputFolder, "wordOutput.docx");
-
-                WordDocumentGenerator.GenerateDocumentByTemplate(templateFilePath, documentData, outputFilePath);
-                string base64 = Base64Helper.ConvertFileToBase64(outputFilePath);
-
-                return Ok(new { message = "Word document generated successfully.", Base64 = base64 });
-            }
-            catch (Exception ex)
+                Placeholders = request.DocumentData.Placeholders,
+                TablesData = request.DocumentData.TablesData
+            };
+            // Generate the doc using the word template
+            string outputFolder = Path.Combine(Directory.GetCurrentDirectory(), "convertedDocs");
+            // Creating a directory for the converted output PDFs if it doesn't exist
+            if (!Directory.Exists(outputFolder))
             {
-                return BadRequest($"Word document generation failed: {ex.Message}");
+                Directory.CreateDirectory(outputFolder);
             }
+
+            string outputFilePath = Path.Combine(outputFolder, "wordOutput.docx");
+
+            WordDocumentGenerator.GenerateDocumentByTemplate(templateFilePath, documentData, outputFilePath);
+            string base64 = Base64Helper.ConvertFileToBase64(outputFilePath);
+
+            return Ok(new { message = "Word document generated successfully.", Base64 = base64 });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Word document generation failed: {ex.Message}");
         }
     }
 }
+
