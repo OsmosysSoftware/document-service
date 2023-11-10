1ce8a10e64a0753faaaa45d48ce22c33ffe92b4a
Shreyansh Jain <shreyansh.j@osmosys.co>
2023-11-10 17:03:55 +0530

    Update word controller to handle base64 template string and document data

--- DocumentServiceWebAPI/Controllers/WordController.cs
+++ DocumentServiceWebAPI/Controllers/WordController.cs
@@ -1,91 +1,69 @@
-﻿using DocumentServiceWebAPI.Models;
-using DocumentService.Word;
-using Microsoft.AspNetCore.Mvc;
+﻿using DocumentService.Word;
 using DocumentServiceWebAPI.Helpers;
+using DocumentServiceWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DocumentServiceWebAPI.Controllers;
 [Route("api")]
 [ApiController]
 public class WordController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _hostingEnvironment;
+
+    public WordController(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
+    {
+        this._configuration = configuration;
+        this._hostingEnvironment = hostingEnvironment;
+    }
+
     [HttpPost]
     [Route("word/GenerateWordDocument")]
     public IActionResult GenerateWord(WordGenerationRequestDTO request)
     {
         try
         {
-            //Common Directories-----------------------------------------------------------------------------
-            string rootFolder = Path.Combine(Directory.GetCurrentDirectory(), "Temp");
-            string inputFolder = Path.Combine(rootFolder, "Input");
-            string outputFolder = Path.Combine(rootFolder, "Output");
+            // Generate filepath to save base64 docx template
+            string docxTemplateFilePath = Path.Combine(
+                this._hostingEnvironment.WebRootPath,
+                this._configuration.GetSection("TEMPORARY_FILE_PATHS:INPUT_WORD").Value,
+                CommonMethodsHelper.GenerateRandomFileName("docx")
+            );
 
-            //Creating Temp folder if not exists
-            if (!Directory.Exists(rootFolder))
+            CommonMethodsHelper.CreateDirectoryIfNotExists(docxTemplateFilePath);
+
+            // Save docx template to inputs directory
+            Base64StringHelper.SaveBase64StringToFilePath(request.Base64, docxTemplateFilePath);
+
+            // Initialize output filepath
+            string outputFilePath = Path.Combine(
+                this._hostingEnvironment.WebRootPath,
+                this._configuration.GetSection("TEMPORARY_FILE_PATHS:OUTPUT_WORD").Value,
+                CommonMethodsHelper.GenerateRandomFileName("docx")
+            );
+
+            CommonMethodsHelper.CreateDirectoryIfNotExists(outputFilePath);
+
+            // Generate and save output docx in output directory
+            WordDocumentGenerator.GenerateDocumentByTemplate(
+                docxTemplateFilePath,
+                request.DocumentData,
+                outputFilePath
+            );
+
+            // Convert docx file in output directory to base64 string
+            string outputBase64String = Base64StringHelper.ConvertFileToBase64String(outputFilePath);
+
+            // Return response
+            return this.Ok(new
             {
-                Directory.CreateDirectory(rootFolder);
-            }
-            //Creating Input folder if not exists
-            if (!Directory.Exists(inputFolder))
-            {
-                Directory.CreateDirectory(inputFolder);
-            }
-            //Creating Output folder if not exists
-            if (!Directory.Exists(outputFolder))
-            {
-                Directory.CreateDirectory(outputFolder);
-            }
-            //Word specific folder structure---------------------------------------------------------------------------
-
-                //Creating Word folder inside Input folder---------------------------------------------------------------
-                string wordInputFolder = Path.Combine(inputFolder, "Word");
-
-                //Creating Word folder if not exists inside Input folder
-                if (!Directory.Exists(wordInputFolder))
-                {
-                    Directory.CreateDirectory(wordInputFolder);
-                }
-                //---------------------------------------------------------------------------------------------------------
-            //Creating Word folder inside Output directory--------------------------------------------------------------
-            string wordOutputFolder = Path.Combine(outputFolder, "Word");
-            //Creating Word folder if not exists inside Input folder
-            if (!Directory.Exists(wordOutputFolder))
-            {
-                Directory.CreateDirectory(wordOutputFolder);
-            }
-            //--------------------------------------------------------------------------------------------------------
-            string ext, randomFileName, randomFileNameWithExtension;
-            ext = "docx";
-            randomFileName = Path.GetRandomFileName().Replace(".", string.Empty);
-            randomFileNameWithExtension = $"{randomFileName}.{ext}";
-
-            string base64FilePath = Path.Combine(wordInputFolder, randomFileNameWithExtension);
-
-            Base64StringHelper.SaveBase64StringToFilePath(request.Base64,base64FilePath );
-            //-------------------------------------------------------------------------------------
-           
-
-            //creating random name for output doc
-            
-            
-            randomFileName = Path.GetRandomFileName().Replace(".", string.Empty);
-            randomFileNameWithExtension = $"{randomFileName}.{ext}";
-            string outputFilePath = Path.Combine(wordOutputFolder, randomFileNameWithExtension);
-
-
-            DocumentService.Word.Models.DocumentData documentData = new()
-            {
-                Placeholders = request.DocumentData.Placeholders,
-                TablesData = request.DocumentData.TablesData
-            };
-
-            WordDocumentGenerator.GenerateDocumentByTemplate(base64FilePath, documentData, outputFilePath);
-            string base64 = Base64StringHelper.ConvertFileToBase64String(outputFilePath);
-
-            return Ok(new { message = "Word document generated successfully.", Base64 = base64 });
+                message = "Word document generated successfully",
+                Base64 = outputBase64String
+            });
         }
         catch (Exception ex)
         {
-            return BadRequest($"Word document generation failed: {ex.Message}");
+            return this.BadRequest($"Word document generation failed: {ex.Message}");
         }
     }
 }
